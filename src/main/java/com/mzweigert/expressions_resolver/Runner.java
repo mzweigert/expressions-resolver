f0f8f61c926519c841d5185fb6c172046cb91b21
mzweigert <mateuszzweigert@gmail.com>
2019-08-16 13:33:20 +0200

    Add basic project structure with configuration files.

---
+++ src/main/java/com/mzweigert/expressions_resolver/Runner.java
@@ -0,0 +1,7 @@
+package com.mzweigert.expressions_resolver;
+
+public class Runner {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/mzweigert/expressions_resolver/RunnerTest.java
@@ -0,0 +1,5 @@
+package com.mzweigert.expressions_resolver;
+
+public class RunnerTest {
+
+}
